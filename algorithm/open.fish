3515f5da00467f011c378dd1075b2b6969be98e2
reicalgrst <240953095+reicalgrst@users.noreply.github.com>
2026-09-23 19:16:56 +0900

    refactor open.fish

--- algorithm/open.fish
+++ algorithm/open.fish
@@ -1,71 +1,79 @@
 #!/usr/bin/env fish
 
-function usage
+function print_usage
     echo "Usage: fish open.fish <contest-id>" >&2
     echo "Example: fish open.fish abc023" >&2
     echo "         fish open.fish adt_all_20260917_1" >&2
 end
 
-if test (count $argv) -ne 1
-    usage
-    exit 2
-end
+function contest_directory --argument-names contest
+    if string match --quiet --regex '^adt_[a-z0-9]+_[0-9]{8}_[0-9]+$' -- $contest
+        set --local parts (string split '_' -- $contest)
+        set --local category $parts[2]
+        set --local year (string sub --start 1 --length 4 -- $parts[3])
 
-set --local contest (string lower -- $argv[1])
-
-if not string match --quiet --regex '^[a-z0-9][a-z0-9_-]*$' -- $contest
-    echo "open.fish: invalid contest ID: $argv[1]" >&2
-    usage
-    exit 2
-end
-
-set --local contest_directory
-
-if string match --quiet --regex '^adt_[a-z0-9]+_[0-9]{8}_[0-9]+$' -- $contest
-    set --local parts (string split '_' -- $contest)
-    set --local category $parts[2]
-    set --local date $parts[3]
-    set --local year (string sub --start 1 --length 4 -- $date)
-    set --local month (string sub --start 5 --length 2 -- $date)
-    set contest_directory "contests/adt/$category/$year/$month/$contest"
-else if string match --quiet --regex '^(abc|arc|agc|ahc)[0-9]{3}$|^awc[0-9]{4}$' -- $contest
-    set --local series (string replace --regex '[0-9]+$' '' -- $contest)
-    set --local digits (string replace --regex '^[a-z]+' '' -- $contest)
-    set --local number (string replace --regex '^0+' '' -- $digits)
-    if test -z "$number"
-        set number 0
+        echo "contests/adt/$category/$year/"(string sub --start 5 --length 2 -- $parts[3])"/$contest"
+        return
     end
 
-    if test "$number" -eq 0
-        echo "open.fish: the contest number must be at least 1" >&2
-        exit 2
+    if string match --quiet --regex '^(abc|arc|agc|ahc)[0-9]{3}$|^awc[0-9]{4}$' -- $contest
+        set --local series (string replace --regex '[0-9]+$' '' -- $contest)
+        set --local number (string replace --regex '^[a-z]+0*' '' -- $contest)
+
+        if test -z "$number"
+            echo "open.fish: the contest number must be at least 1" >&2
+            return 2
+        end
+
+        set --local range_start (math "floor($number / 100) * 100")
+        set --local range_end (math "$range_start + 99")
+        set --local width 3
+        if test "$series" = awc
+            set width 4
+        end
+
+        set --local range (printf "%0*d-%0*d" $width $range_start $width $range_end)
+        echo "contests/$series/$range/$contest"
+        return
     end
 
-    set --local range_start (math "floor($number / 100) * 100")
-    set --local range_end (math "$range_start + 99")
+    echo "contests/othres/$contest"
+end
 
-    if test "$series" = awc
-        set range (printf '%04d-%04d' $range_start $range_end)
-    else
-        set range (printf '%03d-%03d' $range_start $range_end)
+function main
+    if test (count $argv) -ne 1
+        print_usage
+        return 2
     end
 
-    set contest_directory "contests/$series/$range/$contest"
-else
-    set contest_directory "contests/othres/$contest"
+    set --local contest (string lower -- $argv[1])
+
+    if not string match --quiet --regex '^[a-z0-9][a-z0-9_-]*$' -- $contest
+        echo "open.fish: invalid contest ID: $argv[1]" >&2
+        print_usage
+        return 2
+    end
+
+    set --local relative_directory (contest_directory $contest)
+    if test $status -ne 0
+        return 2
+    end
+
+    set --local repository_root (path resolve (status dirname))
+    set --local destination "$repository_root/$relative_directory"
+
+    if not test -d "$destination"
+        cd "$repository_root"; or return 1
+        cargo compete new "$contest"; or return $status
+    end
+
+    if not test -d "$destination"
+        echo "open.fish: cargo-compete did not create $destination" >&2
+        return 1
+    end
+
+    cd "$destination"; or return 1
+    code .
 end
 
-set --local repository_root (path resolve (status dirname))
-set contest_directory "$repository_root/$contest_directory"
-
-cd "$repository_root"; or exit 1
-cargo compete new "$contest"; or exit $status
-
-if not test -d "$contest_directory"
-    echo "open.fish: cargo-compete did not create $contest_directory" >&2
-    exit 1
-end
-
-cd "$contest_directory"; or exit 1
-code .; or exit $status
-wezterm cli kill-pane
+main $argv
